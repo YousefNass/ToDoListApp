92833ef66948c7f0722031956ed229de56c37ed7
Yousef Nasser <yousefnasser@192.168.1.20>
2025-05-31 19:16:48 +0300

    Initial Core Data To-Do List App

---
+++ coreDataExample/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  coreDataExample
+//
+//  Created by Yousef Nasser on 5/31/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
---
+++ coreDataExampleTests/coreDataExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  coreDataExampleTests.swift
+//  coreDataExampleTests
+//
+//  Created by Yousef Nasser on 5/31/25.
+//
+
+import Testing
+@testable import coreDataExample
+
+struct coreDataExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
